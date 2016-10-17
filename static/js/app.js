604e5a7b29f20186fd9b5b0582b838116b29dcc6
Sebastian Schulz <schulz.siwy@gmail.com>
2016-10-17 21:06:54 +0200

    dynamic ver

--- static/js/app.js
+++ static/js/app.js
@@ -1,31 +1,16 @@
 var vm = new Vue({
     el: "#app",
     data: {
-        python: {
-            title: 'Python',
-            data: []
-        },
-        dailyProgrammer: {
-            title: 'DailyProgrammer',
-            data: []
-        },
-        learnPython: {
-            title: 'LearnPython',
-            data: []
-        }
+        type: '',
+        subreddit: []
     },
-    created: function(){
-        this.$http.get("https://www.reddit.com/r/"+this.python.title+"/top.json?limit=5")
-        .then(function(resp){
-            this.python.data = (resp.data.data.children)
-        })
-        this.$http.get("https://www.reddit.com/r/"+this.dailyProgrammer.title+"/top.json?limit=5")
-        .then(function(resp){
-            this.dailyProgrammer.data = (resp.data.data.children)
-        })
-        this.$http.get("https://www.reddit.com/r/"+this.learnPython.title+"/top.json?limit=5")
-        .then(function(resp){
-            this.learnPython.data = (resp.data.data.children)
-        })
+    methods: {
+        getData: function(){
+            var self = this
+            this.$http.get("https://www.reddit.com/r/"+self.type+"/top.json?limit=10")
+            .then(function(resp){
+                this.subreddit = resp.data.data.children
+            })
+        }
     }
 })
--- static/js/components.js
+++ static/js/components.js
@@ -1,13 +1,11 @@
 Vue.component('articles-card', {
     props: {
-        data: Array,
-        title: String
+        data: Array
     },
     template: `
         <div class="col-md-4">
-            <h3 v-text="title"></h3><br>
             <div class="card card-block" v-for="post in data">
-                <p><span class="card-title" v-text="post.data.title"></span><br>
+                <p><b v-text="'('+post.data.ups+') '"></b><span class="card-title" v-text="post.data.title"></span><br>
                 <a class="card-link" :href="post.data.url" v-text="post.data.url.slice(0,50) + '...'"></a></p>
             </div>
         </div>
